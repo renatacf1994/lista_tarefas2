2bc5f2619319cf0f6d226f90dcbb201ad0871bcb
Renata Costa Ferreira <renatacf1994@gmail.com>
2022-05-17 11:18:27 -0300

    configuracao_lista

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'dart:async';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -18,25 +18,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final _toDoController = TextEditingController();
 
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved = {};
+  int _lastRemovedPos = 0;
 
   @override
   void initState() {
     super.initState();
-    _readData().then((data){
-      setState((){
+    _readData().then((data) {
+      setState(() {
         _toDoList = json.decode(data!);
       });
-
     });
   }
 
-  void _addToDo(){
-    setState((){
+  void _addToDo() {
+    setState(() {
       Map<String, dynamic> newToDo = Map();
       newToDo["title"] = _toDoController.text;
       _toDoController.text = "";
@@ -45,63 +45,117 @@ class _HomeState extends State<Home> {
       _saveData();
     });
   }
+  
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState((){
+      _toDoList.sort((a, b){
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Lista de Tarefas"),
-        backgroundColor: Colors.blueAccent,
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-            child: Row(
-              children: [
-                Expanded(
-                    child:TextField(
+        appBar: AppBar(
+          title: Text("Lista de Tarefas"),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
                       controller: _toDoController,
                       decoration: InputDecoration(
                           labelText: "Nova Tarefa",
-                          labelStyle: TextStyle(color: Colors.blueAccent)
-                      ),
+                          labelStyle: TextStyle(color: Colors.blueAccent)),
                     ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
                       primary: Colors.blueAccent,
                       onPrimary: Colors.white,
-                  ),
+                    ),
                     child: Text("ADD"),
                     onPressed: _addToDo,
-                )
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-          Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
+            Expanded(
+              child: RefreshIndicator(onRefresh: _refresh,
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
                   itemCount: _toDoList.length,
-                  itemBuilder: (context, index){
-                    return CheckboxListTile(
-                      title: Text(_toDoList[index]["title"]),
-                      value: _toDoList[index] ["ok"],
-                      secondary: CircleAvatar(
-                          child: Icon(_toDoList[index]["ok"] ?
-                          Icons.check : Icons.error),
-                      ), onChanged: (c) {
-                        setState((){
-                          _toDoList[index]["ok"] = c;
-                          _saveData();
-                        });
-                    },
-                    );
-                  }),
-          )
-        ],
-      )
+                  itemBuilder: buildItem),)
+            )
+          ],
+        ));
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white,),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+              _toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction){
+        setState((){
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: (){
+                setState((){
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              }),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).removeCurrentSnackBar();
+
+          Scaffold.of(context).showSnackBar(snack);
+        });
+
+      },
     );
   }
 
@@ -120,11 +174,8 @@ class _HomeState extends State<Home> {
     try {
       final file = await _getFile();
       return file.readAsString();
-    } catch(e){
+    } catch (e) {
       return null;
     }
-
   }
 }
-
-
